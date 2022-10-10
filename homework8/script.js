d33a36802f29a59091e823e9fc17affa4458818c
amrkany26 <109758580+amrkany26@users.noreply.github.com>
2022-10-11 02:04:19 +0300

    Add files via upload

---
+++ homework8/script.js
@@ -0,0 +1,9 @@
+alert ('welcome to homework 8');
+alert('go straight to console please');
+console.table({user1followlist: 'this list is empty'});
+console.table({you: 'add friends' , user2:' add user 2' , user3:'add user 3', user4: 'add user 4'});
+console.table({yourfollowers:'this list is empty'});
+console.table({yourfollowrequests:'you have 2 requests', user2:'confirm user2' , user3:'confirm user3', user4:'confirm user 4'});
+console.table({user2:'send message to user2', user3:'send message to user 3', user4:'send message to user 4'});
+console.table({user2:'you have 1 unread message', user3:'sent you a voice message', user4:'requests to send you a message'});
+console.table ({ user1: 'no friends', user2 :{inner:'2 friends'}, user3:['4 friends'] , user4: 'no friends'});
